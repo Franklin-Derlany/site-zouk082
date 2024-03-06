f502411f2fc391e3e06a0ba74e004b6f1ef4c179
Franklin-Derlany <franklinderlany@gmail.com>
2024-03-06 16:39:24 -0300

    site oficial zouk082

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var menu = document.querySelector('.menu');
+    var menuToggle = document.querySelector('.menu-toggle');
+    menu.classList.toggle('active');
+    menuToggle.classList.toggle('active'); 
+  }
